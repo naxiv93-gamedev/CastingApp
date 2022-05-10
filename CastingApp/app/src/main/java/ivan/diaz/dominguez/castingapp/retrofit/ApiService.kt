c59b4d57da30cd9568040915f30b492865a7132e
PedroGM80 <pedro13087@gmail.com>
2022-05-10 15:46:43 +0200

    directory retrofit

---
+++ CastingApp/app/src/main/java/ivan/diaz/dominguez/castingapp/retrofit/ApiService.kt
@@ -0,0 +1,15 @@
+package ivan.diaz.dominguez.castingapp
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+
+private const val BASE_URL = "https://ghibliapi.herokuapp.com/"
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
